613d843ab752206a8ee906c79216e468eea8e2e1
Daniel Zamanillo <dzamanillok@gmail.com>
2021-08-23 15:16:30 -0500

    party apis added

--- server.js
+++ server.js
@@ -113,6 +113,87 @@ app.post("/api/candidate", ({ body }, res) => {
 	});
 });
 
+// GET all parties
+app.get("/api/parties", (req, res) => {
+	const sql = `SELECT * FROM parties`;
+	db.query(sql, (err, rows) => {
+		if (err) {
+			res.status(500).json({ error: err.message });
+			return;
+		}
+		res.json({
+			message: "success",
+			data: rows,
+		});
+	});
+});
+
+// Update a candidates party
+app.put("/api/candidate/:id", (req, res) => {
+	const errors = inputCheck(req.body, "party_id");
+
+	if (errors) {
+		res.status(400).json({ error: errors });
+		return;
+	}
+
+	const sql = `UPDATE candidates SET party_id = ?
+                WHERE id = ?`;
+	const params = [req.body.party_id, req.params.id];
+	db.query(sql, params, (err, result) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+		} else if (!result.affectedRows) {
+			res.json({
+				message: "Candidate not found",
+			});
+		} else {
+			res.json({
+				message: "success",
+				data: req.body,
+				changes: result.affectedRows,
+			});
+		}
+	});
+});
+
+// Get party by id
+app.get("/api/party/:id", (req, res) => {
+	const sql = `SELECT * FROM parties WHERE id = ?`;
+	const params = [req.params.id];
+	db.query(sql, params, (err, row) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+		res.json({
+			message: "success",
+			data: row,
+		});
+	});
+});
+
+// Delete a party
+app.delete("/api/party/:id", (req, res) => {
+	const sql = `DELETE FROM parties WHERE id = ?`;
+	const params = [req.params.id];
+	db.query(sql, params, (err, results) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+		} else if (!results.affectedRows) {
+			res.json({
+				message: "Party not found",
+			});
+		} else {
+			res.json({
+				message: "deleted",
+				changes: results.affectedRows,
+				id: req.params.id,
+			});
+		}
+	});
+});
+
 //!  Default res for any other req (Not Found) LAST ROUTE
 app.use((req, res) => {
 	res.status(404).end();
