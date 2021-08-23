347106e45f26603f8c5981be0476f31744a8ada0
Daniel Zamanillo <dzamanillok@gmail.com>
2021-08-23 12:23:37 -0500

    first query

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require("mysql2");
+
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.json({
-		message: "Hello World",
-	});
+// Connect to database
+const db = mysql.createConnection(
+	{
+		host: "localhost",
+		user: "root",
+		password: "goodboy217",
+		database: "election",
+	},
+	console.log("Connected to the election database")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+	console.log(rows);
 });
 
-// Default res for any other req (Not Found)
+//!  Default res for any other req (Not Found) LAST ROUTE
 app.use((req, res) => {
 	res.status(404).end();
 });
